eb3ffb6296fe5f37e1089a931f0ed0362b159ec1
ujued <ujued@qq.com>
2018-07-08 21:51:50 +0800

    优化代码组织

--- src/main/java/net/apisp/quick/server/SocketAutonomy.java
+++ src/main/java/net/apisp/quick/server/SocketAutonomy.java
@@ -31,6 +31,7 @@ import net.apisp.quick.log.Log;
 import net.apisp.quick.log.LogFactory;
 import net.apisp.quick.server.RequestResolver.HttpRequestInfo;
 import net.apisp.quick.server.var.ServerContext;
+import net.apisp.quick.thread.Task;
 import net.apisp.quick.util.Strings;
 
 /**
@@ -40,95 +41,138 @@ import net.apisp.quick.util.Strings;
  * @date 2018-06-12 16:05:50
  */
 public class SocketAutonomy implements SoftCloseable {
-	public static final List<SocketAutonomy> SOCKET_KEEP_LIST = new ArrayList<>(
-			DefaultQuickServer.MAX_SOCKET_KEEP_COUNT);
-	private static final Log LOG = LogFactory.getLog(SocketAutonomy.class);
-	private Socket sock;
-	private Long recentTime;
-	private volatile boolean isInterrupted;
+    public static final List<SocketAutonomy> SOCKET_KEEP_LIST = new ArrayList<>(
+            DefaultQuickServer.MAX_SOCKET_KEEP_COUNT);
+    private static final Log LOG = LogFactory.getLog(SocketAutonomy.class);
+    private Socket sock;
+    private Long recentTime;
+    private volatile boolean isInterrupted;
 
-	public SocketAutonomy(Socket sock) {
-		this.sock = sock;
-		this.recentTime = System.currentTimeMillis();
-	}
+    public SocketAutonomy(Socket sock) {
+        this.sock = sock;
+        this.recentTime = System.currentTimeMillis();
+    }
 
-	/**
-	 * 异步启动自治。（加入受检列表，解析请求，响应请求）
-	 * 
-	 * @param sock
-	 */
-	public static void activeAsync(Socket sock) {
-		SocketAutonomy me = new SocketAutonomy(sock);
-		SOCKET_KEEP_LIST.add(me);
-		DefaultQuickServer.SOCKET_AUTONOMY_EXECUTOR.submit((args) -> {
-			SocketAutonomy sa = (SocketAutonomy) args[0];
-			try {
-				InputStream is = sock.getInputStream();
-				byte[] first = null;
-				int i = 0, n = 0;
-				byte[] buf = new byte[1024 * 10];
-				String reqDataFile = null;
-				while (!sa.isInterrupted) {
-					if ((i = is.read(buf)) == -1) {
-						sock.close();
-						LOG.debug("Read -1, so this socket closed.");
-						break;
-					}
-					if (n == 0) {
-						first = Arrays.copyOfRange(buf, 0, i);
-					} else {
-						if (n == 1) {
-							reqDataFile = Strings.uuid();
-							FileData.create(reqDataFile, ServerContext.tryGet().tmpDirPath(reqDataFile)).persist(first);
-						}
-						FileData.find(reqDataFile).persist(buf, 0, i);
-					}
-					n++; // 更新状态信息
-					if (is.available() == 0) {
-						// 请求数据接受完毕，开始响应
-						DefaultQuickServer.RESPONSE_EXECUTOR.submit((args1) -> {
-							ByteBuffer buffer = (ByteBuffer) args1[0];
-							FileData reqData = (FileData) args1[1];
-							HttpRequestInfo reqInfo = RequestResolver.resolve(buffer).setReqData(reqData)
-									.setInetSocketAddress(sock.getInetAddress());
-							try {
-								OutputStream out = sock.getOutputStream();
-								ResponseExecutor.execute(reqInfo).response(out);
-							} catch (IOException e) {
-								LOG.debug("Socket closed, failed get output stream.");
-							}
-						}, ByteBuffer.wrap(first), FileData.find(reqDataFile));
-						sa.recentTime = System.currentTimeMillis();
-						n = 0;
-						reqDataFile = null;
-					}
-				}
-			} catch (SocketException e) {
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}, me);
-	}
+    /**
+     * 异步启动自治
+     * 
+     * @param sock
+     */
+    public static void activeAsync(Socket sock) {
+        SocketAutonomy socketAutonomy = new SocketAutonomy(sock);
+        // 加入受检列表
+        SOCKET_KEEP_LIST.add(socketAutonomy);
+        // 异步，解析请求，响应请求
+        DefaultQuickServer.SOCKET_AUTONOMY_EXECUTOR.submit(new ReqAndRespResover(sock), socketAutonomy);
+    }
 
-	public void interrupt() {
-		this.isInterrupted = true;
-	}
+    /**
+     * 打断自治
+     */
+    public void interrupt() {
+        this.isInterrupted = true;
+    }
+    
+    /**
+     * 是否停止自治
+     * 
+     * @return
+     */
+    public boolean interrupted() {
+        return this.isInterrupted;
+    }
 
-	public long freeTime() {
-		return System.currentTimeMillis() - recentTime;
-	}
+    /**
+     * 某个Socket多久没使用了，用于自治时判断是否清除
+     * 
+     * @return
+     */
+    public long freeTime() {
+        return System.currentTimeMillis() - recentTime;
+    }
+    
+    /**
+     * 有请求进来
+     */
+    public void flush() {
+        this.recentTime = System.currentTimeMillis();
+    }
 
-	@Override
-	public String toString() {
-		return sock.toString();
-	}
+    @Override
+    public String toString() {
+        return sock.toString();
+    }
 
-	@Override
-	public void close() {
-		try {
-			this.sock.close();
-		} catch (IOException e) {
-			LOG.debug(e);
-		}
-	}
+    @Override
+    public void close() {
+        try {
+            this.sock.close();
+        } catch (IOException e) {
+            LOG.debug(e);
+        }
+    }
+}
+
+/**
+ * 请求与响应协调者
+ * 
+ * @author ujued
+ */
+class ReqAndRespResover implements Task {
+    private static final Log LOG = LogFactory.getLog(ReqAndRespResover.class);
+    private Socket sock;
+
+    public ReqAndRespResover(Socket sock) {
+        this.sock = sock;
+    }
+
+    @Override
+    public void run(Object... args) {
+        SocketAutonomy socketAutonomy = (SocketAutonomy) args[0];
+        try {
+            InputStream is = sock.getInputStream();
+            byte[] first = null;
+            int i = 0, n = 0;
+            byte[] buf = new byte[1024 * 10];
+            String reqDataFile = null;
+            while (!socketAutonomy.interrupted()) {
+                if ((i = is.read(buf)) == -1) {
+                    sock.close();
+                    LOG.debug("Read -1, so this socket closed.");
+                    break;
+                }
+                if (n == 0) {
+                    first = Arrays.copyOfRange(buf, 0, i);
+                } else {
+                    if (n == 1) {
+                        reqDataFile = Strings.uuid();
+                        FileData.create(reqDataFile, ServerContext.tryGet().tmpDirPath(reqDataFile)).persist(first);
+                    }
+                    FileData.find(reqDataFile).persist(buf, 0, i);
+                }
+                n++; // 更新状态信息
+                if (is.available() == 0) {
+                    // 请求数据接受完毕，开始响应
+                    DefaultQuickServer.RESPONSE_EXECUTOR.submit((args1) -> {
+                        ByteBuffer buffer = (ByteBuffer) args1[0];
+                        FileData reqData = (FileData) args1[1];
+                        HttpRequestInfo reqInfo = RequestResolver.resolve(buffer).setReqData(reqData)
+                                .setInetSocketAddress(sock.getInetAddress());
+                        try {
+                            OutputStream out = sock.getOutputStream();
+                            ResponseExecutor.execute(reqInfo).response(out);
+                        } catch (IOException e) {
+                            LOG.debug("Socket closed, failed get output stream.");
+                        }
+                    }, ByteBuffer.wrap(first), FileData.find(reqDataFile));
+                    socketAutonomy.flush();
+                    n = 0;
+                    reqDataFile = null;
+                }
+            }
+        } catch (SocketException e) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
